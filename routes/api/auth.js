bf1e017d9acfe87b40234bcc05105b0428c1816f
Vivek <vukondpalle@gmail.com>
2021-04-07 11:02:59 +0530

    User Authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 // Used for getting JWT for authentication
 
 // @route   GET api/auth
-// @desc    Test Route
+// @desc
 // @access  Public
 router.get('/', auth, async (req, res) => {
   try {
@@ -41,13 +41,17 @@ router.post(
       let user = await User.findOne({ email: email });
 
       if (!user) {
-        return res.status(400).json({ msg: 'Invalid Credentials' });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ msg: 'Invalid Credentials' });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
       const payload = {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Profile = require('../../models/Profile');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
-const request = require("request");
-const config = require("config");
+const request = require('request');
+const config = require('config');
 
 // Anything to do with profiles, fetching, adding, updating, and so on.
 
@@ -318,28 +318,22 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 // @route   GET api/profile/github/:username
 // @desc    Get user repos from Github
 // @access  Private
-router.get("/github/:username", auth, (req,res) => {
+router.get('/github/:username', auth, async (req, res) => {
   try {
-    const options = {
-      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
-      method: 'GET',
-      headers: { 'user-agent': 'node.js' }
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
     };
 
-    request(options, (error, response, body) => {
-      if(error) console.log(error);
-
-      if(response.statusCode !== 200){
-        return res.status(500).json({msg: "No Github profile found"});
-      }
-
-      res.json(JSON.parse(body));
-    })
-
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send("Server Error");
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Github profile found' });
   }
-})
+});
 
 module.exports = router;
